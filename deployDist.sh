b0a954a4b70485e7251044add24de96c3167f222
Rhys Jones <rhysgjones@gmail.com>
2019-04-10 10:53:35 -0600

    Update deployDist script

--- deployDist.sh
+++ deployDist.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-aws s3 sync ./dist/* s3://swagapi.shockwave.com/dist/
+aws s3 sync --acl public-read ./dist/ s3://swagapi.shockwave.com/dist/
+aws cloudfront create-invalidation --distribution-id E1CCE956VLMAE9 --paths /dist/*
