265de3e02b10979709d7660daa55cce9d53cb303
Rhys Jones <rhysgjones@gmail.com>
2019-05-09 15:23:45 -0600

    Add method to parse url params to utils

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,15 @@
 var session = require('session');
 
 var methods = {
+
+  parseUrlParams: function() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+  },
+
   debug: function(message, data) {
     if(session.debug) {
       console.log('SWAG API :::: ' + message);
