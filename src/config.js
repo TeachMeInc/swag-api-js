87f7c11a1ddac036ae1784e3fd7f66c35f9e1c7b
Rhys Jones <rhysgjones@gmail.com>
2019-06-26 10:33:29 -0600

    version 1.1.1

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    version: '1.1.0',
+    version: '1.1.1',
     apiRoot: 'https://swag-services.shockwave.com',
     resourceRoot: 'https://swagapi.shockwave.com/dist/',
     events: {
