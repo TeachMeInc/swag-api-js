2cbc19f1905c250d6d00748c0be7972d45dcd5d6
Rhys Jones <rhysgjones@gmail.com>
2019-06-25 20:03:44 -0600

    Add media breakpoints methods to utils

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,34 @@
 var session = require('session');
+var _ = require('lodash').noConflict();
+
+var mediaBreakpoints = [
+  {name: 'phone', value: 400, class:'media-phone'},
+  {name: 'phablet', value: 560, class:'media-phablet'},
+  {name: 'tablet', value: 768, class:'media-tablet'},
+  {name: 'desktop', value: 1248, class:'media-desktop'}
+];
 
 var methods = {
+
+  getBreakpoint: function() {
+    return _.find(mediaBreakpoints, function(breakpoint) {
+      return session.wrapper.clientWidth <= breakpoint.value;
+    });
+  },
+
+  applyBreakpointClass: function() {
+    var breakpoint = methods.getBreakpoint();
+    if(breakpoint && breakpoint.class) {
+      session.wrapper.dataset.breakpoint = breakpoint.class;
+    } else {
+      session.wrapper.dataset.breakpoint = '';
+    }
+  },
+
+  checkBreakpoint: function(mediaKey) {
+    var breakpoint = methods.getBreakpoint();
+    return breakpoint && (breakpoint.name === mediaKey);
+  },
 
   formatParam: function(param) {
     if(!Array.isArray(param)) {
