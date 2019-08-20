56dcbd33b4d4cc0cf6ab5cd9f340d85a7f4a5e22
Rhys Jones <rhysgjones@gmail.com>
2019-08-20 15:22:46 -0600

    version 1.1.4

--- dist/swag-api.js
+++ dist/swag-api.js
@@ -13,7 +13,7 @@
   !*** ./src/config.js ***!
   \***********************/
 /*! dynamic exports provided */
-/*! all exports used */function(t,e,n){"use strict";t.exports={version:"1.1.3",apiRoot:"https://swag-services.shockwave.com",resourceRoot:"https://swagapi.shockwave.com/dist/",events:{API_COMMUNICATION_ERROR:"API_COMMUNICATION_ERROR",SESSION_READY:"SESSION_READY",DIALOG_CLOSED:"DIALOG_CLOSED",INVALID_DIALOG_TYPE:"INVALID_DIALOG_TYPE",ERROR:"ERROR"}}},
+/*! all exports used */function(t,e,n){"use strict";t.exports={version:"1.1.4",apiRoot:"https://swag-services.shockwave.com",resourceRoot:"https://swagapi.shockwave.com/dist/",events:{API_COMMUNICATION_ERROR:"API_COMMUNICATION_ERROR",SESSION_READY:"SESSION_READY",DIALOG_CLOSED:"DIALOG_CLOSED",INVALID_DIALOG_TYPE:"INVALID_DIALOG_TYPE",ERROR:"ERROR"}}},
 /*!***************************************!*\
   !*** ./node_modules/lodash/lodash.js ***!
   \***************************************/
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    version: '1.1.3',
+    version: '1.1.4',
     apiRoot: 'https://swag-services.shockwave.com',
     resourceRoot: 'https://swagapi.shockwave.com/dist/',
     events: {
